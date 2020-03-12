88492df515c079d81f598ca7aa626d6783e006d0
Blake <blake@futchi.com>
2020-03-12 10:37:15 +0100

    added sad path for non matching string with rspotify api song search

--- spec/requests/api/v1/user_can_search_for_track_spec.rb
+++ spec/requests/api/v1/user_can_search_for_track_spec.rb
@@ -18,11 +18,27 @@ RSpec.describe 'GET /api/v1/tracks', type: :request do
     expect(response_json['tracks'][0]['artist']).to eq 'Khalid'
     end
 
-    describe 'an invalid search' do
+    describe 'an invalid search with empty string' do
         before do
           get '/api/v1/tracks',
           params: {
               q: ' '
+          }
+        end
+
+        it 'returns a 400 response status' do
+        expect(response.status).to eq 400
+        end
+
+        it 'returns a error message' do
+        expect(response_json['song_not_found']).to eq "There is no matches for the song you are trying to search"
+        end
+    end
+    describe 'an invalid search with no matching text' do
+        before do
+          get '/api/v1/tracks',
+          params: {
+              q: 'asdlfjal;ksdfjkl;asdjfkl;asjdflk;ajsdl;fads'
           }
         end
 
