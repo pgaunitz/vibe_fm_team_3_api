199818b1f9623238bf9d28ab5f144bb482003d14
Philip Gaunitz <fluffen@pgaunitz.se>
2020-03-13 22:00:17 +0100

    ads u2 top tracks json

--- app/controllers/api/v1/artists_controller.rb
+++ app/controllers/api/v1/artists_controller.rb
@@ -3,11 +3,12 @@ class Api::V1::ArtistsController < ApplicationController
   
     def index
         artists = RSpotify::Artist.search(params[:q])
-  
+        binding.pry
       sanitized_artists = []
       if artists.empty?
         render json: { error_message: "There is no matches for the artist you are trying to search" }, status: 400
       else
+        
       artists.each do |artist|
         sanitized_artists.push(
           {
