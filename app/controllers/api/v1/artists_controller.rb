8a026a6b7e12c2d3706ebf4e8a63e413727a2165
Jaime <jaime.cruz.hostalot@gmail.com>
2020-03-14 15:03:46 +0100

    user can find top songs of the artist

--- app/controllers/api/v1/artists_controller.rb
+++ app/controllers/api/v1/artists_controller.rb
@@ -3,24 +3,34 @@ class Api::V1::ArtistsController < ApplicationController
   
     def index
         artists = RSpotify::Artist.search(params[:q])
-        binding.pry
       sanitized_artists = []
       if artists.empty?
         render json: { error_message: "There is no matches for the artist you are trying to search" }, status: 400
       else
-        
+    
       artists.each do |artist|
         sanitized_artists.push(
           {
             name: artist.name,
-            genre: artist.genres,
-            followers: artist.followers
+            spotify_id: artist.id,
+            followers: artist.followers,
           }
         )
       end
+
+       artists.first.top_tracks(:SE)[0..10].each do |song|
+          sanitized_artists.push (
+          {
+            song_name: song.name
+          }
+        )
+      end
+
+      binding.pry
       render json: { artists: sanitized_artists }
     end
    end
+   
   
    private
   
